f68c9dff9d1e5c6b8568fa056f4682be79565cfd
jntakpe <j.ntakpe@gmail.com>
2014-10-31 17:52:24 +0100

    Création de la page d'accueil

--- src/main/webapp/static/js/fra.js
+++ src/main/webapp/static/js/fra.js
@@ -1,1 +1,8 @@
 var fraApp = angular.module('fraApp', ['ngMessages', 'ngRoute', 'ngResource', 'ui.bootstrap']);
+
+fraApp.config(function ($routeProvider) {
+    $routeProvider.when('/', {
+        templateUrl: 'views/home.html',
+        controller: 'HomeCtrl'
+    });
+});
