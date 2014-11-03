17616c1398c8e01ffeff416150a74a0ea53ca58c
jntakpe <j.ntakpe@gmail.com>
2014-11-03 23:19:18 +0100

    Button précédent avec directive

--- src/main/webapp/static/js/directives.js
+++ src/main/webapp/static/js/directives.js
@@ -24,3 +24,15 @@ fraApp.directive('endpointAvailableValidator', ['$http', function ($http) {
         }
     };
 }]);
+
+fraApp.directive('back', ['$window', function ($window) {
+    "use strict";
+    return {
+        restrict: 'A',
+        link: function ($scope, element) {
+            element.on('click', function () {
+                $window.history.back();
+            });
+        }
+    };
+}]);
