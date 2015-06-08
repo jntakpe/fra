4b55dec057aeec323b8e59d8c84c53838d462656
jntakpe <j.ntakpe@gmail.com>
2015-06-09 00:11:06 +0200

    Intégration thème Unify

--- src/main/webapp/app/index.js
+++ src/main/webapp/app/index.js
@@ -1,14 +1,3 @@
 'use strict';
 
-angular.module('fra', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ui.router', 'ui.bootstrap'])
-  .config(function ($stateProvider, $urlRouterProvider) {
-    $stateProvider
-      .state('home', {
-        url: '/',
-        templateUrl: 'app/main/main.html',
-        controller: 'MainCtrl'
-      });
-
-    $urlRouterProvider.otherwise('/');
-  })
-;
+angular.module('fra', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ui.router', 'ui.bootstrap']);
