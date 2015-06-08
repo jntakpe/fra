df6d54e0568d5287887069dfe4de30710922b5d4
jntakpe <j.ntakpe@gmail.com>
2015-06-08 23:13:49 +0200

    Génération via Yeoman

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var wrench = require('wrench');
+
+var options = {
+  src: 'src/main/webapp',
+  dist: 'src/main/webapp/dist',
+  tmp: 'src/main/webapp/.tmp',
+  e2e: 'src/main/webapp/e2e',
+  errorHandler: function (title) {
+    return function (err) {
+      gutil.log(gutil.colors.red('[' + title + ']'), err.toString());
+      this.emit('end');
+    };
+  },
+  wiredep: {
+    directory: 'bower_components',
+    exclude: [/bootstrap-sass-official\/.*\.js/, /bootstrap\.css/]
+  }
+};
+
+wrench.readdirSyncRecursive('./gulp').filter(function (file) {
+  return (/\.(js|coffee)$/i).test(file);
+}).map(function (file) {
+  require('./gulp/' + file)(options);
+});
+
+gulp.task('default', ['clean'], function () {
+  gulp.start('build');
+});
