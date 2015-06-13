9fe56e4f970c248e63c35caf0e59d633ae9c02a9
jntakpe <j.ntakpe@gmail.com>
2015-06-13 11:39:04 +0200

    Exemple Tp route modules
    s
    'q' n'est pas reconnu en tant que commande interne
    ou externe, un programme excutable ou un fichier de commandes.

--- src/main/webapp/app/home/home.js
+++ src/main/webapp/app/home/home.js
@@ -1,6 +1,17 @@
 'use strict';
 
 (function () {
+    angular.module('fra-home', ['fra-core']).config(function ($stateProvider) {
+        $stateProvider.state('main.home', {
+            url: '/',
+            views: {
+                'content@layout': {
+                    templateUrl: 'app/home/home.html',
+                    controller: 'HomeCtrl as home'
+                }
+            }
+        });
+    });
 
 })();
 
