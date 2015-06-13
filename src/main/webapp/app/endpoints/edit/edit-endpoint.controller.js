3286f6c8b5cc63c8a0c20da4b1f9afe4fe7d35a6
jntakpe <j.ntakpe@gmail.com>
2015-06-13 16:44:55 +0200

    Breadcrumb

--- src/main/webapp/app/endpoints/edit/edit-endpoint.controller.js
+++ src/main/webapp/app/endpoints/edit/edit-endpoint.controller.js
@@ -2,7 +2,6 @@
 
 (function () {
     angular.module('fra-endpoints').controller('EditEndpointsCtrl', function () {
-        var vm = this;
 
     });
 })();
--- src/main/webapp/app/endpoints/endpoints.js
+++ src/main/webapp/app/endpoints/endpoints.js
@@ -4,6 +4,10 @@
     angular.module('fra-endpoints', ['fra-core']).config(function ($stateProvider) {
         $stateProvider.state('main.endpoints', {
             url: '/endpoints',
+            data: {
+                breadcrumb: ['main.home'],
+                title: 'Liste des endpoints'
+            },
             views: {
                 'content@layout': {
                     templateUrl: 'app/endpoints/endpoints.html',
