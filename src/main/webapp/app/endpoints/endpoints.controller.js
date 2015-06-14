f8dfa16f01c3566b4eabc450114ee3c43686629f
jntakpe <j.ntakpe@gmail.com>
2015-06-14 15:55:50 +0200

    TP services - step5 visualize modal (cherry picked from commit ed29244)

--- src/main/webapp/app/endpoints/endpoints.controller.js
+++ src/main/webapp/app/endpoints/endpoints.controller.js
@@ -1,12 +1,24 @@
 'use strict';
 
 (function () {
-    angular.module('fra-endpoints').controller('EndpointsCtrl', function (endpoints, EndpointsService) {
+    angular.module('fra-endpoints').controller('EndpointsCtrl', function (EndpointsService, endpoints, $modal) {
         var vm = this;
 
         vm.endpoints = endpoints;
         vm.remove = function (endpoint) {
             endpoint.remove().then(refresh);
+        };
+        vm.visualize = function (endpoint) {
+            $modal.open({
+                templateUrl: 'app/endpoints/json-view-modal.html',
+                controller: 'JsonViewModalCtrl as jsonView',
+                size: 'lg',
+                resolve: {
+                    endpoint: function () {
+                        return endpoint;
+                    }
+                }
+            });
         };
 
         function refresh() {
---
+++ src/main/webapp/app/endpoints/visualize-modal.controller.js
@@ -0,0 +1,15 @@
+'use strict';
+
+(function () {
+    angular.module('fra-endpoints').controller('JsonViewModalCtrl', function (endpoint, $modalInstance) {
+        var vm = this;
+
+        vm.endpoint = {
+            uri: endpoint.uri,
+            content: JSON.parse(endpoint.content)
+        };
+        vm.close = function () {
+            $modalInstance.close();
+        }
+    });
+})();
