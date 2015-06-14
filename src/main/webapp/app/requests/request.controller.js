d85ec373a64a5cf87278b02b5b08467ea6e76376
jntakpe <j.ntakpe@gmail.com>
2015-06-14 13:33:33 +0200

    Exemple requests (cherry picked from commit b7d5cfd)

--- src/main/webapp/app/requests/request.controller.js
+++ src/main/webapp/app/requests/request.controller.js
@@ -1,7 +1,18 @@
 'use strict';
 
 (function () {
-    angular.module('fra-requests').controller('RequestsCtrl', function () {
+    angular.module('fra-requests').controller('RequestsCtrl', function (RequestsService, requests) {
+        var vm = this;
+
+        vm.requests = requests.data;
+
+        vm.clear = function () {
+            RequestsService.clear().success(function () {
+                RequestsService.findAll().success(function (data) {
+                    vm.requests = data;
+                });
+            });
+        };
 
     });
 })();
