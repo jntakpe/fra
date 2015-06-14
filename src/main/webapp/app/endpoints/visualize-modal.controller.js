de7579c154bd7520d93e3ec6e9b449b18356b87a
jntakpe <j.ntakpe@gmail.com>
2015-06-14 20:14:59 +0200

    Using real data

--- src/main/webapp/app/endpoints/visualize-modal.controller.js
+++ src/main/webapp/app/endpoints/visualize-modal.controller.js
@@ -4,7 +4,10 @@
     angular.module('fra-endpoints').controller('JsonViewModalCtrl', function (endpoint, $modalInstance) {
         var vm = this;
 
-        vm.endpoint = endpoint;
+        vm.endpoint = {
+            uri: endpoint.uri,
+            content: JSON.parse(endpoint.content)
+        };
         vm.close = function () {
             $modalInstance.close();
         };
