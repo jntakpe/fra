d0eb29be41f98f49629ef4a2bd8a24179ad329f6
jntakpe <j.ntakpe@gmail.com>
2015-06-14 15:14:58 +0200

    TP Services - step4 delete endpoints

--- src/main/webapp/app/endpoints/endpoints.controller.js
+++ src/main/webapp/app/endpoints/endpoints.controller.js
@@ -5,6 +5,16 @@
         var vm = this;
 
         vm.endpoints = endpoints;
+        vm.remove = function (endpoint) {
+            endpoint.remove().then(function () {
+                refresh()
+            });
+        };
 
+        function refresh() {
+            EndpointsService.baseEndpoints.getList().then(function (data) {
+                vm.endpoints = data;
+            });
+        }
     });
 })();
