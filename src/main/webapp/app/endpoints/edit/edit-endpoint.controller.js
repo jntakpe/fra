075917b4946550b5d2ef051de5ddeac288ddd151
jntakpe <j.ntakpe@gmail.com>
2015-06-25 23:08:07 +0200

    Enregistrement d'endpoint

--- src/main/webapp/app/endpoints/edit/edit-endpoint.controller.js
+++ src/main/webapp/app/endpoints/edit/edit-endpoint.controller.js
@@ -1,10 +1,25 @@
 'use strict';
 
 (function () {
-    angular.module('fra-endpoints').controller('EditEndpointsCtrl', function (endpoint) {
+    angular.module('fra-endpoints').controller('EditEndpointsCtrl', function (endpoint, EndpointsService, $state) {
         var vm = this;
 
+        var backToList = function () {
+            $state.go('main.endpoints', {}, {reload: true});
+        };
+
         vm.endpoint = endpoint;
+        if (!endpoint.id) {
+            vm.endpoint.method = 'GET';
+        }
+
+        vm.submit = function () {
+            if (endpoint.id) {
+                vm.endpoint.put().then(backToList);
+            } else {
+                EndpointsService.baseEndpoints.post(vm.endpoint).then(backToList);
+            }
+        };
 
     });
 })();
