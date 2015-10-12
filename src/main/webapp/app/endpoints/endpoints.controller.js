c21c866c7d467db5e66722afc873da38158cc7d4
jntakpe <j.ntakpe@gmail.com>
2015-10-12 22:03:35 +0200

    TP Services - step4 delete endpoints

--- src/main/webapp/app/endpoints/endpoints.controller.js
+++ src/main/webapp/app/endpoints/endpoints.controller.js
@@ -1,9 +1,18 @@
 'use strict';
 
 (function () {
-    angular.module('fra-endpoints').controller('EndpointsCtrl', function (endpoints) {
+    angular.module('fra-endpoints').controller('EndpointsCtrl', function (endpoints, EndpointsService) {
         var vm = this;
 
         vm.endpoints = endpoints;
+        vm.remove = function (endpoint) {
+            endpoint.remove().then(refresh);
+        };
+
+        function refresh() {
+            EndpointsService.list().then(function (data) {
+                vm.endpoints = data;
+            });
+        }
     });
 })();
